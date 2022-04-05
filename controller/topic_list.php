80288a495e1e156a4e247f3101d34c5773169a2c
Alyyen <alyssia.colomar@gmail.com>
2022-04-05 16:48:35 +0200

    php : return topics by category as json to javascript

--- controller/topic_list.php
+++ controller/topic_list.php
@@ -2,10 +2,19 @@
 	require_once('../model/database.php');
 	require_once('../model/configuration.php');
 
+	$topics_array = [];
 	$object = new Database();
 	$id_category = $_POST['data'];
+	
 	// GET DATAS FROM DATABASE
-	$list_mobile_topics = $object->get_topics_by_idcategory($id_category);
-	foreach ($list_mobile_topics as $topic){
-		print_r($topic);
+	$list_all_topics = $object->get_topics_by_idcategory($id_category);
+
+	// RETURN DATAS AS JSON
+	if ($list_all_topics != NULL) {
+		echo json_encode(array("status" => 'Success', "id_category" => $id_category,
+			"datas" => $list_all_topics));
+	} else {
+		header("HTTP/1.0 404 Not Found");
+		die(json_encode(array("status" => 'error')));
 	}
+
