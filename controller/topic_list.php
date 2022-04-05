7c99ddfb5499e17da1300d97099816d73e70d6ab
Alyyen <alyssia.colomar@gmail.com>
2022-04-05 17:23:51 +0200

    php : delete useless variables

--- controller/topic_list.php
+++ controller/topic_list.php
@@ -1,8 +1,7 @@
 <?php
 	require_once('../model/database.php');
 	require_once('../model/configuration.php');
-
-	$topics_array = [];
+	
 	$object = new Database();
 	$id_category = $_POST['data'];
 	
@@ -17,4 +16,5 @@
 		header("HTTP/1.0 404 Not Found");
 		die(json_encode(array("status" => 'error')));
 	}
+	
 
