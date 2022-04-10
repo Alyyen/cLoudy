203028efc7269e8afb2e9dd94f57527a859beb35
Alyyen <alyssia.colomar@gmail.com>
2022-04-10 23:42:59 +0200

    js : fix error message if not data found when new post sent

--- assets/scripts/new_post_script.js
+++ assets/scripts/new_post_script.js
@@ -11,23 +11,36 @@ function getFormData($form) {
 }
 
 $(function () {
+
     // SEND NEW POST DATAS TO DATABASE
     $("#new-post-form").submit(function (event) {
-
+        // ERROR IF NO TITLE OR CONTENT WRITTEN
+        $("#error_new_post_title").empty();
+        $("#error_new_post_content").empty();
         event.preventDefault();
-        let form = $(this);
+        if ($("#new-post-content-id").val() == "" || $("#new-post-title-id").val() == "") {
+            if ($("#new-post-content-id").val() == "") {
+                $("#error_new_post_content").append("<span class='text-danger'>Please enter a content for your post.</span>");
+            }
+            if ($("#new-post-title-id").val() == "") {
+                $("#error_new_post_title").append("<span class='text-danger'>Please enter a title for your post.</span>");
+            }
+        } else {
+            // POST DATAS WHEN NO EMPTY INPUTS
+            let form = $(this);
 
-        $.ajax({
-            url: "../controller/topic.php",
-            type: "post",
-            dataType: 'json',
-            data: {
-                action: "new-post",
-                data: getFormData(form),
-            },
-            error: function (jqxhr) {
-                console.log(jqxhr.responseText);
-            },
-        });
+            $.ajax({
+                url: "../controller/topic.php",
+                type: "post",
+                dataType: 'json',
+                data: {
+                    action: "new-post",
+                    data: getFormData(form),
+                },
+                error: function (jqxhr) {
+                    console.log(jqxhr.responseText);
+                },
+            });
+        }
     });
 });
